e61b88fe9f19c3101688417a69a969701ae5abfa
kadavid08 <kovacsdavid480@gmail.com>
2026-03-26 10:40:15 +0100

    push

---
+++ value.js
@@ -0,0 +1,7 @@
+function szamol() {
+    console.log("Hello");
+    let szelesseg = document.getElementById("szelesseg")
+    let hossz = document.getElementById("hossz")
+
+    console.log(szelesseg)
+    console.log(hossz)
